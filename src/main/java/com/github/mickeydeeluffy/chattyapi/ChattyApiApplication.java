7748750fdfafeb4c0af006927f430cd190bd2138
MickeyDLuffy <thefalconslayer@gmail.com>
2022-03-27 21:27:21 +0100

    Initial commit

---
+++ src/main/java/com/github/mickeydeeluffy/chattyapi/ChattyApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mickeydeeluffy.chattyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mickeydeeluffy/chattyapi/ChattyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mickeydeeluffy.chattyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
